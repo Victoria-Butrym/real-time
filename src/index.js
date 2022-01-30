c6d0be813fcc9a4fbeb06bf05dde28509586a389
Victoria Butrym <victoria-butrym@tut.by>
2022-01-30 17:46:10 +0300

    1.0 [real-time] added conncet/disconnect handlers

--- src/index.js
+++ src/index.js
@@ -19,4 +19,10 @@ io.on('connection', (socket) => {
         console.log('client: ', message);
         io.emit('message', message);
     })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+
+        io.emit('message', 'user disconnected')
+    })
 })
